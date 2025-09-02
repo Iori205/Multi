8ef003963b01ac2cb10b293201aa4a3b86b0a5d6
Iori205 <b.sodbilegt11@gmail.com>
2025-09-02 12:51:20 +0800

    Step2

--- multi-state/src/app/page.js
+++ multi-state/src/app/page.js
@@ -1,35 +1,98 @@
-"use client"
-import { useState } from "react"
-import {Input, Text, Button} from "@/components"
+"use client";
+import { useState } from "react";
+import { Input, Text, Button } from "@/components";
 
-export default function Home () {
-
+export default function Home() {
   const [form, setForm] = useState({
     email: "",
     phone: "",
     firstName: "",
     lastName: "",
-    userName: ""
-  })
+    userName: "",
+  });
+  const [step, setStep] = useState("Step1");
 
-  return (
-  <div className="bg-[#F4F4F4] w-full h-screen pt-[182px] flex justify-center">
-    <div className="bg-[#FFFFFF] w-[480px] h-fit border-transparent rounded-lg p-[32px]">
-      <header>
-        <img src="ima/Main 1.svg"/>
-        <p className="text-[#202124] text-[26px] font-semibold py-[8px]">Join Us! 😎</p>
-        <p className="text-[#8E8E8E] text-[18px] font-normal">Please provide all current information accurately.</p>
-      </header>
-      <div className="pt-[28px]">
-        <Text>First name</Text>
-        <Input value={form.firstName} onChange={(e) => setForm({...form,firstName: e.target.value})} />
-        <Text>Last name</Text>
-        <Input/>
-        <Text>Username</Text>
-        <Input/>
+  function submit() {
+    setStep("Step2");
+  }
+
+  if (step === "Step1") {
+    return (
+      <div className="bg-[#F4F4F4] w-full h-screen pt-[182px] flex justify-center">
+        <div className="bg-[#FFFFFF] w-[480px] h-fit border-transparent rounded-lg p-[32px]">
+          <header>
+            <img src="ima/Main 1.svg" />
+            <p className="text-[#202124] text-[26px] font-semibold py-[8px]">
+              Join Us! 😎
+            </p>
+            <p className="text-[#8E8E8E] text-[18px] font-normal">
+              Please provide all current information accurately.
+            </p>
+          </header>
+          <div className="pt-[28px]">
+            <Text>First name</Text>
+            <Input
+              value={form.firstName}
+              onChange={(e) => setForm({ ...form, firstName: e.target.value })}
+            />
+            <Text>Last name</Text>
+            <Input
+              value={form.lastName}
+              onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+            />
+            <Text>Username</Text>
+            <Input
+              value={form.userName}
+              onChange={(e) => setForm({ ...form, userName: e.target.value })}
+            />
+          </div>
+          <Button onClick={submit}>Continue 1/3 </Button>
+        </div>
       </div>
-      <Button>Continue</Button>
-    </div>
-  </div>
-  )
+    );
+  }
+
+  if (step === "Step2") {
+    return (
+      <div className="bg-[#F4F4F4] w-full h-screen pt-[182px] flex justify-center">
+        <div className="bg-[#FFFFFF] w-[480px] h-fit border-transparent rounded-lg p-[32px]">
+          <header>
+            <img src="ima/Main 1.svg" />
+            <p className="text-[#202124] text-[26px] font-semibold py-[8px]">
+              Join Us! 😎
+            </p>
+            <p className="text-[#8E8E8E] text-[18px] font-normal">
+              Please provide all current information accurately.
+            </p>
+          </header>
+          <div className="pt-[28px]">
+            <Text>Email</Text>
+            <Input
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+            <Text>Phone number</Text>
+            <Input
+              value={form.phone}
+              onChange={(e) => setForm({ ...form, phone: e.target.value })}
+            />
+            <Text>Password</Text>
+            <Input
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+            <Text>Confirm password</Text>
+            <Input
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+          <div>
+            <Button variant="secondary"></Button>
+            <Button variant="primary">Continue 2/3</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- multi-state/src/components/input.jsx
+++ multi-state/src/components/input.jsx
@@ -1,20 +1,31 @@
-export const Text = ({children}) => {
-    return (
-        <p className="{`text-[#334155] text-[14px] font-semibold pb-[8px] leading-[16px]`}">{children} <span className="text-[#E14942] text-[14px]">*</span></p>
-    )
-}
+export const Text = ({ children }) => {
+  return (
+    <p className="text-[#334155] text-[14px] font-semibold pb-[8px] leading-[16px]">
+      {children} <span className="text-[#E14942] text-[14px]">*</span>
+    </p>
+  );
+};
 
 export const Input = () => {
-    return (
-        <div>
-            <input className="w-[416px] h-[44px] border border-solid border-[#CBD5E1] rounded-[8px] text-[#8B8E95] font-normal font-[16px] pl-[13px] mb-[12px]"
-            placeholder="Placeholder"/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input
+        className="w-[416px] h-[44px] border border-solid border-[#CBD5E1] rounded-[8px] text-[#8B8E95] font-normal font-[16px] pl-[13px] mb-[12px]"
+        placeholder="Placeholder"
+      />
+    </div>
+  );
+};
 
-export const Button = ({children}) => {
-    return (
-        <button className="w-[416px] h-[44px] bg-[#121316] border rounded-md mt-[150px] text-white">{children}</button>
-    )
-}
+export const Button = ({ variant, children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`w-[416px] h-[44px] bg-[#121316] border rounded-md mt-[150px] text-white ${
+        variant === "primary" ? "bg-red-400" : ""
+      } ${variant === "secondary" ? "bg-yellow-100" : ""}`}
+    >
+      {children}
+    </button>
+  );
+};
